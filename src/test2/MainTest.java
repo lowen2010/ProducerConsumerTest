ee2440a7ebb2b97ad127ba3b6d22952ed0929970
lowen <lowen2010@126.com>
2018-01-24 15:17:16 +0800

    增加线程池测试

--- src/test2/MainTest.java
+++ src/test2/MainTest.java
@@ -1,7 +1,11 @@
 package test2;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainTest {
 
@@ -9,10 +13,28 @@ public class MainTest {
 		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(5);
 		Consumer consumer =  new Consumer(queue);
 		Producer producer =  new Producer(queue);
+//		for (int i = 0; i < 15; i++) {
+//			new Thread(consumer).start();
+//			new Thread(producer).start();
+//		}
+		ExecutorService threadPool = Executors.newCachedThreadPool();
+//		ExecutorService threadPool = Executors.newFixedThreadPool(3);
+		
 		for (int i = 0; i < 15; i++) {
-			new Thread(consumer).start();
-			new Thread(producer).start();
+			threadPool.execute(consumer);
+			threadPool.execute(producer);
 		}
+		threadPool.shutdown();
+		
+		ScheduledExecutorService schThreadPool = Executors.newScheduledThreadPool(5);
+		schThreadPool.schedule(producer, 1, TimeUnit.SECONDS);
+		schThreadPool.schedule(consumer, 1, TimeUnit.SECONDS);
+		schThreadPool.shutdown();
+		
+		ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();
+		singleThreadPool.execute(producer);
+		singleThreadPool.execute(consumer);
+		singleThreadPool.shutdown();
 
 	}
 
--- src/test2/Producer.java
+++ src/test2/Producer.java
@@ -1,10 +1,13 @@
 package test2;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Producer implements Runnable{
 
 	private BlockingQueue<String> queue;
+	
+	private AtomicInteger count = new AtomicInteger(0);
 	
 	public Producer(BlockingQueue<String> queue) {
 		this.queue = queue;
@@ -13,7 +16,7 @@ public class Producer implements Runnable{
 	@Override
 	public void run() {
 		try {
-			String prodName = Thread.currentThread().getName();
+			String prodName = Thread.currentThread().getName() + ",count=" +count.incrementAndGet();
 			queue.put(prodName);
 			System.out.println("生产产品："+prodName);
 		} catch (InterruptedException e) {
